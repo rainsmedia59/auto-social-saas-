35347fe973e0c3fd552e4420c4e4a2cf1789065d
rainsmedia59 <rainsmedia59@gmail.com>
2026-04-26 16:07:26 +0100

    Create index.js

---
+++ frontend/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Auto Social SaaS</h1>
+      <p>Connect your website using API key</p>
+    </div>
+  );
+}
