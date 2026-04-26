97eac5745eb287981fa846291ff884682c2dedbf
rainsmedia59 <rainsmedia59@gmail.com>
2026-04-26 17:30:29 +0100

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,9 +1,12 @@
 export function auth(req, res, next) {
   const key = req.headers["x-api-key"];
 
-  if (!key || key !== "demo_key_123") {
+  const user = users.find(u => u.apiKey === key);
+
+  if (!user) {
     return res.status(403).json({ error: "Invalid API Key" });
   }
 
+  req.user = user;
   next();
 }
