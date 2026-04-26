1018c71ec623cb724a76f54e00ad8be101437e78
rainsmedia59 <rainsmedia59@gmail.com>
2026-04-26 18:09:21 +0100

    Create login.js

---
+++ frontend/pages/login.js
@@ -0,0 +1,12 @@
+export default function Login() {
+  return (
+    <div style={{ padding: 50 }}>
+      <h1>Login</h1>
+
+      <input placeholder="Email" style={{ display: "block", margin: 10 }} />
+      <input placeholder="Password" style={{ display: "block", margin: 10 }} />
+
+      <button>Login</button>
+    </div>
+  );
+}
